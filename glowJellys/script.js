fb08b480963b33dad2ded7cc8f3250314a3d5732
Daniel Preil <90137943+DanielPreil@users.noreply.github.com>
2022-09-17 21:20:56 +0200

    Content kiépítése, scroll animációk kidolgozva (Ha Content bővül, akkor a legördülés gombok alapján történő Függvények futtatása nem léesz tökéletes (Megoldás: akkor majd változtatni kell a pixelek érték nagyságát)), shortCutIcon tökéletesebb crop-olása, hibás css-html id nevek átírása, css rendezett, kommentek rendben vannak

---
+++ glowJellys/script.js
@@ -0,0 +1,23 @@
+// ----- Scroll Függvények -----
+
+function topFunction1() {
+  document.body.scrollTop = 0; // Safari Böngésző Igénylésére
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE és Opera Böngészők Igénylésére
+}
+
+function topFunction2() {
+    document.body.scrollTop = 125; // Safari Böngésző Igénylésére
+    document.documentElement.scrollTop = 125; // Chrome, Firefox, IE és Opera Böngészők Igénylésére
+}
+
+function topFunction3() {
+    document.body.scrollTop = 600; // Safari Böngésző Igénylésére
+    document.documentElement.scrollTop = 600; // Chrome, Firefox, IE és Opera Böngészők Igénylésére
+}
+
+function topFunction4() {
+    document.body.scrollTop = 1075; // Safari Böngésző Igénylésére
+    document.documentElement.scrollTop = 1075; // Chrome, Firefox, IE és Opera Böngészők Igénylésére
+}
+
+// ----- Scroll Függvények Vége -----
