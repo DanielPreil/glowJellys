036fb4e765579e3ffba216f04e42167fe16deeef
Daniel Preil <90137943+DanielPreil@users.noreply.github.com>
2022-09-17 21:26:13 +0200

    navBar onClick Fügvény nevek rendes funció alapján igényesebb és pontosabb nevet kaptak, az alapján amit csinálnak futtatáskor.

--- glowJellys/script.js
+++ glowJellys/script.js
@@ -1,21 +1,21 @@
 // ----- Scroll Függvények -----
 
-function topFunction1() {
+function scrollToTheTop() {
   document.body.scrollTop = 0; // Safari Böngésző Igénylésére
   document.documentElement.scrollTop = 0; // Chrome, Firefox, IE és Opera Böngészők Igénylésére
 }
 
-function topFunction2() {
+function scrollToTheFirstTitle() {
     document.body.scrollTop = 125; // Safari Böngésző Igénylésére
     document.documentElement.scrollTop = 125; // Chrome, Firefox, IE és Opera Böngészők Igénylésére
 }
 
-function topFunction3() {
+function scrollToTheSecondTitle() {
     document.body.scrollTop = 600; // Safari Böngésző Igénylésére
     document.documentElement.scrollTop = 600; // Chrome, Firefox, IE és Opera Böngészők Igénylésére
 }
 
-function topFunction4() {
+function scrollToTheThirdTitle() {
     document.body.scrollTop = 1075; // Safari Böngésző Igénylésére
     document.documentElement.scrollTop = 1075; // Chrome, Firefox, IE és Opera Böngészők Igénylésére
 }
